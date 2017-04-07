23b1db818f834b4c866391c1f0787f0aeef0d1a8
Steven <steven_moxon123@hotmail.co.uk>
2017-04-07 15:58:50 +0100

    added smooth scroll

--- script.js
+++ script.js
@@ -8,4 +8,18 @@ $(document).ready(function(){
     	$(this).css('background-color', '');
     	$(this).css('color', '');
 	});
+
+	$('a[href^="#"]').on('click',function (e) {
+		    e.preventDefault();
+
+	    	var target = this.hash;
+	    	var $target = $(target);
+
+	    	$('html, body').stop().animate({
+	        	'scrollTop': $target.offset().top
+	    	}, 900, 'swing', function () {
+	        	window.location.hash = target;
+	    	});
+	});
+    	
 });
